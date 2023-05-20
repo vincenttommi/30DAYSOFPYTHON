47ba8e11ac113e6ecf9d7a9069b8291e3da09ff4
vincenttommi <vincenttommi@gmail.com>
2023-05-20 13:06:58 +0300

    adding sum of numbers in python

---
+++ Day4/day4.py
@@ -0,0 +1,11 @@
+# a is the list , it adds up all the numbers in the 
+ #list a and takes start to be 0, so returning 
+# only the sum of the numbers in the list.
+# sum(a, start)
+# this returns the sum of the list + start 
+
+
+numbers = [200,100,200,300,400,12]
+
+sum = (numbers)
+print(sum)

bea872e28e18236829b56cc8a55b958cdc6f48f5
vincenttommi <vincenttommi@gmail.com>
2023-05-20 13:25:09 +0300

    printing second largest value in a set

--- Day4/day4.py
+++ Day4/day4.py
@@ -5,7 +5,23 @@
 # this returns the sum of the list + start 
 
 
-numbers = [200,100,200,300,400,12]
+numbers = [200,100,200,300,400,199]
 
 sum = (numbers)
 print(sum)
+
+#Program to find the second largest number in an  list
+# assigning  values first in a list
+list1 = [20,30,40,2,50,70,40]
+
+# Removing Duplicates in the  list
+list2 = list(set(list1))
+#It creates a set from the elements in list1 using the set() function. A set is an unordered collection of unique elements, meaning it removes any duplicate values from list1
+# then converts the set back into a list using the list() function and assigns the result to the variable list2.
+
+
+# sorting the list
+list2.sort()
+
+
+print("The seceond largest number in list2 is :", list2[-2])

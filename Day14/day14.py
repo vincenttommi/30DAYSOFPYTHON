929b2f9f92373be01c9e90c243cde983408dd6cf
vincenttommi <vincenttommi@gmail.com>
2023-06-16 15:28:32 +0300

    day 14 completed

--- Day14/day14.py
+++ Day14/day14.py
@@ -6,3 +6,26 @@ with open('Day14/data.json','r')  as file:
     data  = json.load(file)
       
     print(data)
+
+
+
+
+with open('Day14/books.json','r') as file:
+    
+    books = json.load(file)
+    
+#iterating over  each book and printing the title and author
+
+for book in  books:
+    
+    # author = book['author']: This line assigns the value of the 'author' key from the book dictionary to the variable author. It retrieves the author information of the current book.
+    title  =  book['title']
+    author  = book['author']
+    
+    print("Title:", title)
+    print("Author:", author)
+    
+    print()
+    
+    # for printing all
+    # print(book)

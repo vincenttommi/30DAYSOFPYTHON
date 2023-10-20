858351de7a4da3cb84020b3fea94a7dc6ad51e7f
vincenttommi <vincenttommi@gmail.com>
2023-10-20 13:33:17 +0300

    crud performed

--- Day20/app.py
+++ Day20/app.py
@@ -48,3 +48,60 @@ task = Task(title="Fullstack-Developer", user=user)
 session.add(user)
 session.add(task)
 session.commit()
+
+#retrieving data using sqlalchemy
+users =  session.query(User).all()
+
+#Retrieve a single user by id
+user = session.query(User).get(1)
+
+
+#filtering and  sorting query  results  with sqlalchemy
+# users = session.query(User).filter_by(email="vincenttommi@gmail.com").all()
+
+
+#Retreive users whose  usernames start with 'j and sort by username
+# users = session.query(User).filter(User.username.startswith("j")).order_by(User.username).all()
+
+
+# updating Data(in CRUD)
+#steps
+"""
+1 Query the record I need to update
+2 Modify the attributes of  queried object
+3  Commit  session to save changes to database
+"""
+
+# Query a user by id
+user =  session.query(User).get(2)
+
+
+#updating the user's email
+user.email = "vincent@gmail.com"
+
+
+# committing the changes
+session.commit()
+
+
+
+
+#Deleting Data in (CRUD)
+"""steps
+
+
+Query the record you want to delete
+Use session's delete method to remove the object
+Commit  session to finalize deletion
+
+"""
+
+#Querying a user  by id
+user = session.query(User).get(6)
+user = session.query(User).get(7)
+
+
+#Delete the user
+
+session.delete(user)
+session.commit()
